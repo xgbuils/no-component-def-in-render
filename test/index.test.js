75235d0283a8f3b6d9a9b5e0b38f6724f9577efd
Xavier Garcia Buils <xgbuils@gmail.com>
2023-09-02 13:20:49 +0200

    test: ensure nested component ternary declaration is not valid

--- test/index.test.js
+++ test/index.test.js
@@ -3,11 +3,13 @@ const hocTests = require("./hoc.test.js");
 const hooksTests = require("./hooks.test.js");
 const mapTests = require("./map.test.js");
 const renderFnTests = require("./renderFn.test.js");
+const ternaryTests = require('./ternary.test.js');
 
 basicTests.run();
 hooksTests.run();
 hocTests.run();
 mapTests.run();
 renderFnTests.run();
+ternaryTests.run();
 
 console.log("All tests passed!");
