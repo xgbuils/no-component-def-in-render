f325280d6abd146e0a5923a5985d539f52e4bd40
Xavier Garcia Buils <xgbuils@gmail.com>
2024-01-02 00:02:15 +0100

    apply tests to several EcmaScript versions

--- test/ruleTester/index.js
+++ test/ruleTester/index.js
@@ -7,50 +7,57 @@ import { createReport } from "./createReport.js";
 
 const removeDescription = ({ description, ...props }) => props;
 
-const createTestGroup = ({ valid, invalid, file } = {}) => {
+const createTestGroup = ({ valid, invalid, file, versions } = {}) => {
 	const errors = [];
-	const ruleTester = new RuleTester({
-		// Must use at least ecmaVersion 2015 because
-		// that's when `const` variables were introduced.
-		parserOptions: {
-			ecmaVersion: 2015,
-			ecmaFeatures: { jsx: true },
-			sourceType: "module",
-		},
-	});
+	const ruleTesters = {};
+	const getRuleTester = (version) =>
+		ruleTesters[version] ??
+		new RuleTester({
+			// Must use at least ecmaVersion 2015 because
+			// that's when `const` variables were introduced.
+			parserOptions: {
+				ecmaVersion: version,
+				ecmaFeatures: { jsx: true },
+				sourceType: "module",
+			},
+		});
 
-	const runRuleTest = (type) => (useCase, index) => {
-		const cases = {
-			valid: [],
-			invalid: [],
-			[type === "valid" ? "valid" : "invalid"]: [removeDescription(useCase)],
+	const runRuleTest = (type, version) => {
+		const ruleTester = getRuleTester(version);
+		return (useCase, index) => {
+			const cases = {
+				valid: [],
+				invalid: [],
+				[type === "valid" ? "valid" : "invalid"]: [removeDescription(useCase)],
+			};
+			try {
+				ruleTester.run(
+					"restricted-nested-component-def", // rule name
+					noNestedComponentDefinition, // rule code
+					cases,
+				);
+			} catch (error) {
+				errors.push(
+					createReport(error, {
+						...useCase,
+						file,
+						type,
+						index,
+					}),
+				);
+			}
 		};
-		try {
-			ruleTester.run(
-				"restricted-nested-component-def", // rule name
-				noNestedComponentDefinition, // rule code
-				cases,
-			);
-		} catch (error) {
-			errors.push(
-				createReport(error, {
-					...useCase,
-					file,
-					type,
-					index,
-				}),
-			);
-		}
 	};
 
 	return {
 		run() {
 			try {
-				valid.forEach(runRuleTest("valid"));
-				invalid.forEach(runRuleTest("invalid"));
+				versions.forEach((version) => {
+					valid.forEach(runRuleTest("valid", version));
+					invalid.forEach(runRuleTest("invalid", version));
+				});
 				return errors;
 			} catch (error) {
-				console.error("eyy", error);
 				return [];
 			}
 		},
@@ -69,13 +76,16 @@ const createRuleTester = () => {
 						valid = [],
 						invalid = [],
 						parsing = [],
+						versions = [2015, 2020],
 					} = await import(filePath);
-					const numberOfTests = valid.length + invalid.length;
+					const numberOfTests =
+						(valid.length + invalid.length) * versions.length;
 					const errors = createTestGroup({
 						file,
 						valid,
 						invalid,
 						parsing,
+						versions,
 					}).run();
 					if (errors.length > 0) {
 						hasErrors = true;
